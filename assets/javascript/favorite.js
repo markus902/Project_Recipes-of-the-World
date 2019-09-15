c9e7cc998e11be5e161c069dd6e788aab9093c20
Markus Markowich <markus902@gmx.de>
2019-09-14 23:55:14 -0400

    prepared adding favorites to list

--- assets/javascript/favorite.js
+++ assets/javascript/favorite.js
@@ -1,40 +1,23 @@
-$(function() {
+$(function () {
 
     // Load the favorites from localstorage.
     let favoriteList = [];
 
     // get favorite list from local storage
-    function getFavoriteList() {
-        if (typeof(Storage) !== "undefined") {
-            if (localStorage.getItem("favorites") != null) {
-                favoriteList = JSON.parse(localStorage.getItem("favorites"));
-                console.log(typeof(favoriteList));
-            }
-        } else {
-            // Sorry! No Web Storage support..
-        }
-    }
+    var database = firebase.database()
 
-    // render favorites to page
-    function renderFavorites(favoriteList) {
-        for (var i = 0; i < favoriteList.length; i++) {
-            let favorite = $("<li>");
-            favorite.text(favoriteList[i].title);
-            console.log(favoriteList[i].title);
+    database.ref(`/users/${sessionStorage.getItem("user")}`).on("child_added", function (snapshot) {
+        snapshot.forEach(function () {
+            let favorite = $("<p>");
+            favorite.text(snapshot.val().recipeID);
             $("#favoriteRecipe").append(favorite);
-        }
-    }
+        });
+    })
 
-    $(document).on("click", "#favoriteRecipe", function(event) {
+    $(document).on("click", "#favoriteRecipe", function (event) {
         event.preventDefault();
-        localStorage.setItem("ingredients", JSON.stringify($(this).attr("value")));
         window.location.href = "./ingredients.html"
     });
 
-    // call get favorites 
-    getFavoriteList();
-
-    // call render favorites function 
-    renderFavorites(favoriteList);
 
 });
